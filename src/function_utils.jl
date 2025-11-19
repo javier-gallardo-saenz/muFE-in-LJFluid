8eb3c84e62c00444434743bc7431d2457ddf91d1
javier-gallardo-saenz <jgallardosaenz@gmail.com>
2025-11-19 23:25:40 +0100

    more functions added to function_utils

--- src/function_utils.jl
+++ src/function_utils.jl
@@ -51,7 +51,7 @@ end
 """
 Softened interaction potential derivative wrt λ evaluation
 """
-function soft_pot_dr(r::R, params::LJ_params, λ::R) where {R<:Real}
+function soft_pot_dλ(r::R, params::LJ_params, λ::R) where {R<:Real}
     aux = (r/params.σ)^6
     A = 0.5*(1-λ) + aux
     return 2*params.ϵ*(2*A^(-3) - A^(-2))
@@ -166,8 +166,8 @@ function dH_pi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}
 
     @inbounds for i in 1:N-1
         #compute derivative of interaction potential wrt the inserted particle, fixed at the origin
-        r0 = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
-        dHdq[i] .+= λ*LJ_pot_der(r0, params)*q[i]/r0
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*LJ_pot_der(ri, params)*q[i]/ri
 
         for j in i+1:N
             #compute radius and distance vector between particles i and j
@@ -207,9 +207,9 @@ function dH_pi_dq_and_dλ(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SV
     @inbounds for i in 1:N-1
         #compute derivative of interaction potential with the inserted particle, fixed at the origin
         #and derivative of interaction potential wrt λ
-        r0 = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
-        dHdq[i] .+= λ*LJ_pot_der(r0, params)*q[i]/r0
-        dHdλ += LJ_pot(r0, params)
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*LJ_pot_der(ri, params)*q[i]/ri
+        dHdλ += LJ_pot(ri, params)
 
         for j in i+1:N
             #compute radius and distance vector between particles i and j
@@ -238,7 +238,7 @@ q-gradient and U(q) of the particle insertion Hamiltonian, H = T(p) + U(q)
 Assumes q is given as Vector{SVector{d, R}} with length(q) = N
 Periodic boundary conditions with the standard cutoff L/2 are imposed.
 """
-function V_and_dH_pi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
+function u_and_dH_pi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
     d, N = length(q[1]), length(q)
     @assert d == length(L) "The boxes dimensions must match the dimension of q"
     T = typeof(norm(@view q[:,1]))
@@ -250,9 +250,9 @@ function V_and_dH_pi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVec
     @inbounds for i in 1:N-1
         #compute derivative of interaction potential with the inserted particle, fixed at the origin
         #and contribution of the λ-term to V
-        r0 = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
-        dHdq[i] .+= λ*LJ_pot_der(r0, params)*q[i]/r0
-        U += λ*LJ_pot(r0, params)
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*LJ_pot_der(ri, params)*q[i]/ri
+        U += λ*LJ_pot(ri, params)
 
         for j in i+1:N
             #compute radius and distance vector between particles i and j
@@ -271,7 +271,6 @@ function V_and_dH_pi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVec
     rN = sqrt(sum(abs2, q[N]))
     dHdq[N] .+= λ*LJ_pot_der(rN, params)*q[N]/rN
     U += λ*LJ_pot(rN, params)
-    dHdλ += LJ_pot(rN, params)
 
     return dHdq, U
     
@@ -296,10 +295,10 @@ function U_and_dH_pi_dq_and_dλ(q::Vector{SVector{R}}, params::LJ_params, λ::R,
     @inbounds for i in 1:N-1
         #compute derivative of interaction potential with the inserted particle, fixed at the origin
         #and contribution of the λ-term to V
-        r0 = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
-        dHdq[i] .+= λ*LJ_pot_der(r0, params)*q[i]/r0
-        U += λ*LJ_pot(r0, params)
-        dHdλ += LJ_pot(r0, params)
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*LJ_pot_der(ri, params)*q[i]/ri
+        U += λ*LJ_pot(ri, params)
+        dHdλ += LJ_pot(ri, params)
 
         for j in i+1:N
             #compute radius and distance vector between particles i and j
@@ -318,8 +317,9 @@ function U_and_dH_pi_dq_and_dλ(q::Vector{SVector{R}}, params::LJ_params, λ::R,
     rN = sqrt(sum(abs2, q[N]))
     dHdq[N] .+= λ*LJ_pot_der(rN, params)*q[N]/rN
     U += λ*LJ_pot(rN, params)
+    dHdλ += LJ_pot(rN, params)
 
-    return dHdq, U
+    return dHdq, dHdλ, U
     
 end
 
@@ -344,6 +344,179 @@ function H_spi(p::AbstractMatrix{R}, q::AbstractMatrix{R}, m::R, params::LJ_para
     V_orig = sum(LJ_pot.(pw_dist, Ref(params)))  
     V_inserted = λ*sum(soft_pot.(norm.(eachcol(q)), Ref(params), Ref(λ))) 
     return K + V_orig + V_inserted
+end
+
+
+"""
+q-gradient of the softened particle insertion Hamiltonian.
+Assumes q is given as Vector{SVector{d, R}} with length(q) = N
+Periodic boundary conditions with the standard cutoff L/2 are imposed.
+"""
+function dH_spi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
+    d, N = length(q[1]), length(q)
+    @assert d == length(L) "The boxes dimensions must match the dimension of q"
+    T = typeof(norm(@view q[:,1]))
+
+    dHdq = [@MVector zeros(T, d) for _ in 1:N]
+    dq = MVector{d, T}(undef)
+
+    @inbounds for i in 1:N-1
+        #compute derivative of interaction potential wrt the inserted particle, fixed at the origin
+        r0 = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*soft_pot_dr(r0, params, λ)*q[i]/r0
+
+        for j in i+1:N
+            #compute radius and distance vector between particles i and j
+            dq .= q[i] .- q[j]
+            dq .= dq .- L .* round.(dq./ L) #pbc + minimum image convention
+            r  = sqrt(sum(abs2, dq))
+
+            #compute gradient
+            Fij = LJ_pot_der(r, params)*dq/r
+            dHdq[i] .+= Fij
+            dHdq[j] .-= Fij
+        end
+    end
+
+    rN = sqrt(sum(abs2, q[N]))
+    dHdq[N] .+= λ*soft_pot_dr(rN, params, λ)*q[N]/rN
+
+    return dHdq
+    
+end
+
+
+"""
+q-gradient and λ-gradient of the softened particle insertion Hamiltonian.
+Assumes q is given as Vector{SVector{d, R}} with length(q) = N
+Periodic boundary conditions with the standard cutoff L/2 are imposed.
+"""
+function dH_spi_dq_and_dλ(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
+    d, N = length(q[1]), length(q)
+    @assert d == length(L) "The boxes dimensions must match the dimension of q"
+    T = typeof(norm(@view q[:,1]))
+
+    dHdq = [@MVector zeros(T, d) for _ in 1:N]
+    dHdλ = 0
+    dq = MVector{d, T}(undef)
+
+    @inbounds for i in 1:N-1
+        #compute derivative of interaction potential with the inserted particle, fixed at the origin
+        #and derivative of interaction potential wrt λ
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*soft_pot_dr(ri, params, λ)*q[i]/r0
+        dHdλ += soft_pot(ri, params, λ) + soft_pot_dλ(ri, params, λ)
+
+        for j in i+1:N
+            #compute radius and distance vector between particles i and j
+            dq .= q[i] .- q[j]
+            dq .= dq .- L .* round.(dq./ L) #pbc + minimum image convention
+            r  = sqrt(sum(abs2, dq))
+
+            #compute gradient
+            Fij = LJ_pot_der(r, params)*dq/r
+            dHdq[i] .+= Fij
+            dHdq[j] .-= Fij
+        end
+    end
+
+    rN = sqrt(sum(abs2, q[N]))
+    dHdq[N] .+= λ*soft_pot_dr(rN, params, λ)*q[N]/rN
+    dHdλ += soft_pot(rN, params, λ) + soft_pot_dλ(rN, params, λ)
+
+    return dHdq, dHdλ
+    
+end
+
+
+"""
+q-gradient and U(q) of the softened particle insertion Hamiltonian, H = T(p) + U(q)
+Assumes q is given as Vector{SVector{d, R}} with length(q) = N
+Periodic boundary conditions with the standard cutoff L/2 are imposed.
+"""
+function U_and_dH_spi_dq(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
+    d, N = length(q[1]), length(q)
+    @assert d == length(L) "The boxes dimensions must match the dimension of q"
+    T = typeof(norm(@view q[:,1]))
+
+    dHdq = [@MVector zeros(T, d) for _ in 1:N]
+    U = 0
+    dq = MVector{d, T}(undef)
+
+    @inbounds for i in 1:N-1
+        #compute derivative of interaction potential with the inserted particle, fixed at the origin
+        #and contribution of the λ-term to V
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*soft_pot_dr(ri, params, λ)*q[i]/ri
+        U += λ*soft_pot(r0, params, λ)
+
+        for j in i+1:N
+            #compute radius and distance vector between particles i and j
+            dq .= q[i] .- q[j]
+            dq .= dq .- L .* round.(dq./ L) #pbc + minimum image convention
+            r  = sqrt(sum(abs2, dq))
+
+            #compute gradient and contribution to V
+            Fij = LJ_pot_der(r, params)*dq/r
+            dHdq[i] .+= Fij
+            dHdq[j] .-= Fij
+            U += LJ_pot(r, params)
+        end
+    end
+
+    rN = sqrt(sum(abs2, q[N]))
+    dHdq[N] .+= λ*soft_pot_dr(rN, params, λ)*q[N]/rN
+    U += λ*soft_pot(rN, params, λ)
+
+    return dHdq, U
+    
+end
+
+
+"""
+q-gradient, λ-gradient and U(q) of the softened particle insertion Hamiltonian, H = T(p) + U(q)
+Assumes q is given as Vector{SVector{d, R}} with length(q) = N
+Periodic boundary conditions with the standard cutoff L/2 are imposed.
+"""
+function U_and_dH_pi_dq_and_dλ(q::Vector{SVector{R}}, params::LJ_params, λ::R, L::SVector{R}) where {R<:Real}
+    d, N = length(q[1]), length(q)
+    @assert d == length(L) "The boxes dimensions must match the dimension of q"
+    T = typeof(norm(@view q[:,1]))
+
+    dHdq = [@MVector zeros(T, d) for _ in 1:N]
+    dHdλ = 0
+    U = 0
+    dq = MVector{d, T}(undef)
+
+    @inbounds for i in 1:N-1
+        #compute derivative of interaction potential with the inserted particle, fixed at the origin
+        #and contribution of the λ-term to V
+        ri = sqrt(sum(abs2, q[i])) #no need to worry about minimum image convention here
+        dHdq[i] .+= λ*soft_pot_dr(ri, params, λ)*q[i]/ri
+        dHdλ += soft_pot(ri, params, λ) + soft_pot_dλ(ri, params,λ)
+        U += λ*soft_pot(r0, params, λ)
+
+        for j in i+1:N
+            #compute radius and distance vector between particles i and j
+            dq .= q[i] .- q[j]
+            dq .= dq .- L .* round.(dq./ L) #pbc + minimum image convention
+            r  = sqrt(sum(abs2, dq))
+
+            #compute gradient and contribution to V
+            Fij = LJ_pot_der(r, params)*dq/r
+            dHdq[i] .+= Fij
+            dHdq[j] .-= Fij
+            U += LJ_pot(r, params)
+        end
+    end
+
+    rN = sqrt(sum(abs2, q[N]))
+    dHdq[N] .+= λ*soft_pot_dr(rN, params, λ)*q[N]/rN
+    dHdλ += soft_pot(rN, params, λ) + soft_pot_dλ(rN, params,λ)
+    U += λ*soft_pot(rN, params, λ)
+
+    return dHdq, dHdλ, U
+    
 end
 
 
